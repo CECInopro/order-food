62cb5ea9623945e3169a2d8fde8115b88c5248f8
Đỗ Thành Công <dothanhcong1011@gmail.com>
2025-12-15 07:00:17 +0700

    first commit, add home, nav

---
+++ src/page/Home.jsx/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import Headers from '../../component/Header/Header.jsx'
+const Home = () => {
+    return (
+        <div>
+            <Headers />
+
+        </div>
+    )
+}
+
+export default Home
