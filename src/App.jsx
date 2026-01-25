32b6ecd81d712c1b78d7758191ce1bdd550e9165
Đỗ Thành Công <dothanhcong1011@gmail.com>
2026-01-25 21:54:44 +0700

    add footer, add menu

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
     <div className='app'>
       <Navbar />
       <Routes>
+        
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/place-order' element={<PlaceOrder />} />
